ee1ef958791a7d94145ea3be85126d042b0d4a56
Rob Curtis <surreymagpie@yahoo.co.uk>
2014-04-23 14:55:01 +0100

    Added Google Analytic code

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,28 +1,40 @@
 <!DOCTYPE html>
 <html <?php print $html_attributes;?> lang="en">
-	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<meta charset="utf-8" />
-		<meta name="description" content="" />
-		<meta name="author" content="Robert Curtis" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="shortcut icon" href="favicon.ico" />
-		<script>document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/';</script>
-		<link href='http://fonts.googleapis.com/css?family=PT+Sans|PT+Serif:400,700' rel='stylesheet' type='text/css' />
-		<title><?php print $head_title; ?></title>
-		<?php print $head; ?>
-		<?php print $styles; ?>
-		<!--[if lte IE 8]>
-			<link href="<?php print $base_url;?>/sites/all/themes/dentist/stylesheets/ie.css" type="text/css" rel="stylesheet"/>
-		<![endif]-->
- 		</head>
-	<body class="<?php print $classes; ?>" <?php print $attributes; ?>>
-		<div id="skip-link">
-			<a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-		</div>
-		<?php print $page_top; ?>
-		<?php print $page; ?>
-		<?php print $page_bottom; ?>
-		<?php print $scripts; ?>
-	</body>
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="author" content="Robert Curtis" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="favicon.ico" />
+    <script>document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/';</script>
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans|PT+Serif:400,700' rel='stylesheet' type='text/css' />
+    <title><?php print $head_title; ?></title>
+    <?php print $head; ?>
+    <?php print $styles; ?>
+    <!--[if lte IE 8]>
+      <link href="<?php print $base_url;?>/sites/all/themes/dentist/stylesheets/ie.css" type="text/css" rel="stylesheet"/>
+    <![endif]-->
+    <!-- Google Analytics -->
+      <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-44028720-1', 'auto');
+      ga('require', 'displayfeatures');
+      ga('send', 'pageview');
+      </script>
+    <!-- End Google Analytics -->
+  </head>
+  <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
+    <div id="skip-link">
+      <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    </div>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
+    <?php print $scripts; ?>
+  </body>
 </html>
