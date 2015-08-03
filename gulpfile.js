bb6e60540d4e625bc9ca320b97ac4f7a870639c2
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-08-03 13:57:32 +0100

    Setup gulp to compile CSS and reload browser

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var shell = require('gulp-shell');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+var src = {
+    scss: 'sass/**/*.scss',
+    css: 'stylesheets',
+    php: '**/*php',
+    inc: '**/*.inc'
+};
+
+gulp.task('sass', function() {
+    gulp.src(src.scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', '> 1%'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(src.css))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        proxy: "drupal.dev",
+    });
+
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch([src.php, src.inc], ['theme', browserSync.reload]);
+});
+
+gulp.task('theme', function() {
+    return shell.task([
+        'drush cc theme-registry',
+    ])
+});
+
+gulp.task('default', ['serve']);
