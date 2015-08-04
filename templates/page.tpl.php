90dddc092c019aa22e4294b09ba043c35def5c56
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-08-04 08:25:19 +0100

    Change to page template started

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,34 +1,43 @@
 <div class="container">
+
+  <header class="site__header">
+    <?php if($logo): ?>
+    <div id="logo">
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    </div>
+    <?php endif; ?>
+
+    <?php if ($page['navbar']): ?>
+    <nav class="navbar">
+      <?php print render($page['navbar']); ?>
+    </nav>
+    <?php endif ?>
+
+    <?php if($site_name): ?>
+    <div id="site-name">
+      <h2><?php print $site_name; ?></h2>
+    </div>
+    <?php endif; ?>
+
+    <?php if($site_slogan): ?>
+    <div id="site-slogan">
+      <h2><?php print $site_slogan; ?></h2>
+    </div>
+    <?php endif; ?>
+
+    <?php if ($page['header']): ?>
+      <?php print render($page['header']); ?>
+    <?php endif ?>
+
+  </header>
   <!-- for small screens the checkbox toggles the menu -->
   <input type="checkbox" id="menu">
-  <div class="user">
-    <!-- the uppermost div housing the user menu -->
-    <?php print render($page['user']); ?>
-  </div>
 
   <div id="page">
     <div class="header">
       <label for="menu" onclick></label> <!-- for the checkbox -->
-
-      <?php if($logo): ?>
-			<div id="logo">
-			  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-			    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-			  </a>
-			</div>
-      <?php endif; ?>
-
-      <?php if($site_slogan): ?>
-      <div id="site-slogan">
-        <h2><?php print $site_slogan; ?></h2>
-      </div>
-      <?php endif; ?>
-
-      <?php print render($page['header']); ?>
-      <div class="nav">
-        <?php print render($page['nav']); ?>
-      </div>
-
       <?php if($breadcrumb){print $breadcrumb;}?>
 
       <?php print $messages; ?> <!-- status and error messages appear here -->
@@ -41,7 +50,7 @@
       </div>
       <?php endif; ?>
 
-      <div id="main-content">
+      <main id="main-content">
         <div id="content">
           <?php print render($title_prefix); ?>
           <?php if($title): ?>
@@ -79,7 +88,7 @@
 				<?php print render($page['sidebar_right']); ?>
 				</div> <!--/#sidebar_right -->
 				<?php endif; ?>
-      </div> <!-- /#main-content -->
+      </main> <!-- /#main-content -->
     </div> <!-- .wrapper -->
   </div> <!-- /#page -->
 
