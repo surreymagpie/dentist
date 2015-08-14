96cb06d259a678f983180e36b17b3c2ffba69582
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-08-14 20:53:48 +0100

    Move image preprocess function into template.php
    as would not work in separate file.

--- template.php
+++ template.php
@@ -1,12 +1,12 @@
 <?php
 
 function dentist_form_alter(&$form, &$form_state, $form_id) {
+
   //search box customisation//
-  //if ($form_id == 'search_block_form') {
+  if ($form_id == 'search_block_form') {
   // HTML5 placeholder attribute
-  $form['search_block_form']['#attributes']['placeholder'] = t('Enter a search term...');
-	$form['search_block_form']['#attributes']['size'] = t('15');
-  //}
+    $form['search_block_form']['#attributes']['placeholder'] = t('Enter a search term...');
+  }
 }
 
 
@@ -16,6 +16,15 @@ include_once 'includes/node.inc';
 
 include_once 'includes/block.inc';
 
-include_once 'includes/image.inc';
-
 include_once 'includes/css.inc';
+
+/**
+*   Implements theme_proprocess_hook
+*/
+function dentist_preprocess_image(&$variables) {
+
+  // remove width and height attributes so that we can style with CSS
+  foreach (array('width', 'height') as $key) {
+    unset($variables[$key]);
+  }
+}
