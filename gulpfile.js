b0cc4410f4036a81028a33f16d42571c481a837d
Rob Curtis <surreymagpie@yahoo.co.uk>
2022-09-16 09:42:18 +0100

    Remove node-sass as dev dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-const { src, dest, series, watch } = require ('gulp');
-let sass = require('gulp-sass');
-sass.compiler = require ('node-sass');
+const { src, dest, series, watch } = require('gulp');
+let sass = require('gulp-sass')(require('sass'));
+// sass.compiler = ;
 let autoprefixer = require('gulp-autoprefixer');
 let browserSync = require('browser-sync').create();
 let source = {
-    scss: 'sass/**/*.scss',
-    css: 'stylesheets',
-    php: '**/*php',
-    inc: '**/*.inc'
+  scss: 'sass/**/*.scss',
+  css: 'stylesheets',
+  php: '**/*php',
+  inc: '**/*.inc'
 };
 
 function styles() {
-    return src(source.scss)
-        .pipe(sass({outputStyle: "compressed" }).on('error', sass.logError))
-        .pipe(autoprefixer({ browsers: ['last 2 versions', '> 1%'] }))
-        .pipe(dest(source.css))
-        .pipe(browserSync.stream());
+  return src(source.scss)
+    .pipe(sass({ outputStyle: "compressed" }).on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['last 2 versions', '> 1%'] }))
+    .pipe(dest(source.css))
+    .pipe(browserSync.stream());
 };
 
 function serve() {
-    browserSync.init({
-        proxy: "http://localhost:8090",
-    });
-    watch(source.scss, styles);
+  browserSync.init({
+    proxy: "http://drupal7.ddev.site",
+  });
+  watch(source.scss, styles);
 }
 
 exports.default = series(styles, serve);
